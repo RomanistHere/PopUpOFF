21369f1d362121271dff60387cc1fbb358e12bfe
romanisthere@gmail.com <romanisthere@gmail.com>
2019-08-10 12:47:57 +0300

    minify 1.1.2

--- content.js
+++ content.js
@@ -1,14 +1,1 @@
-'use strict';
-
-// local variable for state of current page (do not work after reload)
-var	thisPageOn = false
-
-chrome.runtime.onMessage.addListener(function (msg, sender, response) {
-	if (msg.method === "getStatusThisPage") {
-		response(thisPageOn)
-	}
-	if (msg.method === "setStatusThisPage") {
-		thisPageOn = msg.thisPageOn
-	}
-	return true
-})
+"use strict";var thisPageOn=!1;chrome.runtime.onMessage.addListener(function(e,t,s){return"getStatusThisPage"===e.method&&s(thisPageOn),"setStatusThisPage"===e.method&&(thisPageOn=e.thisPageOn),!0});
