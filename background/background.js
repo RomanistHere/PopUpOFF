a0227b89c8894d995b1285b0490711c1acf7b31d
romanisthere <romanisthere@gmail.com>
2020-10-07 17:14:12 +0300

    add context menu activation

--- background/background.js
+++ background/background.js
@@ -77,47 +77,62 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	}
 })
 
+const checkAndRunMode = (tabId, pureUrl, sendResponse) => {
+	storageGet(['supervision', 'shortCutMode'], (res) => {
+		const mode = res.shortCutMode
+		if (res.supervision && ARR_OF_FORB_SITES.includes(pureUrl)) {
+			sendResponse({ shouldShow: false })
+		} else if (mode) {
+			storageGet(['thisWebsiteWork', 'thisWebsiteWorkEasy'], (res) => {
+				// need to check both arrays
+				const isHard = (mode == 'thisWebsiteWork') ? true : false
+				const arrOfSites = res[isHard ? 'thisWebsiteWork' : 'thisWebsiteWorkEasy']
+				const oppArrOfSites = res[isHard ? 'thisWebsiteWorkEasy' : 'thisWebsiteWork']
+
+				if (!arrOfSites.includes(pureUrl)) {
+					sendResponse({ shouldShow: true })
+
+					const newArrOfSites = [...arrOfSites, pureUrl]
+					storageSet({ [mode]: newArrOfSites })
+
+					activateMode(isHard)(tabId)
+					if (oppArrOfSites.includes(pureUrl)) {
+						// check if website is in opposite mode array
+						const newOppArrOfSites = oppArrOfSites.filter(e => e !== pureUrl)
+						storageSet({[isHard ? 'thisWebsiteWorkEasy' : 'thisWebsiteWork']: newOppArrOfSites})
+					}
+				} else {
+					sendResponse({ shouldShow: false })
+
+					const newArrOfSites = arrOfSites.filter(e => e !== pureUrl)
+					storageSet({[mode]: newArrOfSites})
+
+					executeScript(tabId)('restore')
+					resetBadgeText(tabId)
+				}
+			})
+		}
+	})
+}
+
+chrome.contextMenus.removeAll()
+chrome.contextMenus.create({
+	title: `Toggle PopUpOFF`,
+	onclick: (obj, tabs) => {
+		const pureUrl = getPureURL(tabs)
+		const tabId = tabs.id
+		const fakeShouldResp = () => null
+		checkAndRunMode(tabId, pureUrl, fakeShouldResp)
+	}
+})
+
 // messages from content script. Currently responsible for 'alt + x' key comb
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (sender.tab) {
 		const tabId = sender.tab.id
 		const pureUrl = getPureURL(sender)
 		if (request.hardMode) {
-			storageGet(['supervision', 'shortCutMode'], (res) => {
-				const mode = res.shortCutMode
-				if (res.supervision && ARR_OF_FORB_SITES.includes(pureUrl)) {
-					sendResponse({ shouldShow: false })
-				} else if (mode) {
-					storageGet(['thisWebsiteWork', 'thisWebsiteWorkEasy'], (res) => {
-						// need to check both arrays
-						const isHard = (mode == 'thisWebsiteWork') ? true : false
-						const arrOfSites = res[isHard ? 'thisWebsiteWork' : 'thisWebsiteWorkEasy']
-						const oppArrOfSites = res[isHard ? 'thisWebsiteWorkEasy' : 'thisWebsiteWork']
-
-						if (!arrOfSites.includes(pureUrl)) {
-							sendResponse({ shouldShow: true })
-
-					    	const newArrOfSites = [...arrOfSites, pureUrl]
-					        storageSet({ [mode]: newArrOfSites })
-
-							activateMode(isHard)(tabId)
-				    		if (oppArrOfSites.includes(pureUrl)) {
-				    			// check if website is in opposite mode array
-				    			const newOppArrOfSites = oppArrOfSites.filter(e => e !== pureUrl)
-				    			storageSet({[isHard ? 'thisWebsiteWorkEasy' : 'thisWebsiteWork']: newOppArrOfSites})
-				    		}
-					    } else {
-							sendResponse({ shouldShow: false })
-
-							const newArrOfSites = arrOfSites.filter(e => e !== pureUrl)
-							storageSet({[mode]: newArrOfSites})
-
-							executeScript(tabId)('restore')
-							resetBadgeText(tabId)
-						}
-					})
-				}
-			})
+			checkAndRunMode(tabId, pureUrl, sendResponse)
 		} else if (request.openOptPage) {
 			chrome.runtime.openOptionsPage()
 		}
